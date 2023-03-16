34e98f51397191e5829b7f7a1da93bd3963854e6
512187 <512187@vistacollege.nl>
2023-03-16 08:53:08 +0100

    w

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("form");
 eField = form.querySelector(".email"),
 eInput = eField.querySelector("input"),
-pField = form.querySelector(".password"),
+pField = form.querySelector(".password"), 
 pInput = pField.querySelector("input");
 form.onsubmit = (e)=>{
   e.preventDefault(); //preventing from form submitting
